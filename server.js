43976e7f010b06b4b6889a3bb117327ed3e193b0
Glitch (uptimebotx) <none>
2020-11-27 20:37:37 +0000

    🌶🗽 Checkpoint ./server.js:23759784/118

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("express")().listen(1343);//ukqzn
 const db = require("quick.db"); 
 const discord = require("discord.js");//ukqzn
 const client = new discord.Client({ disableEveryone: true });//ukqzn
-client.login("NzIzODUyMTM4NjEwMDMyNzIw.Xu3p1g.EUKQvnXuqDDJwovUvk6VIW_QQXI");
+client.login("NzIzODUyMTM4NjEwMDMyNzIw.Xu3p1g.DZc6fUqaeVu3bp6pqZ3Kt59wiOc");
 const fetch = require("node-fetch");
 const fs = require('fs')//ukqzn
 
